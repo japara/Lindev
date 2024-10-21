4128b10250de08dccd172a4d03db73fb7ff7e9bf
japara <72559893+japara@users.noreply.github.com>
2024-10-21 23:11:29 +0400

    education component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import About from "./components/About";
+import Education from "./components/Education";
 
 function App() {
   return (
     <div>
       <Header />
       <About />
+      <Education />
     </div>
   );
 }
---
+++ src/components/Education.jsx
@@ -0,0 +1,23 @@
+function Education() {
+  return (
+    <div className="flex justify-center items-center mt-[161px]">
+      <img src="/Educ.svg"></img>
+      <div className=" flex justify-items-start  flex-col">
+        <p className="bg-gradient-to-b from-[#2662D6] to-[#3DB8B1] text-transparent bg-clip-text font-bold text-[36px] text-left mb-[20px]">
+          Our Education System <br /> Works For You
+        </p>
+        <p className="text-left mb-[60px] text-[18px] font-normal">
+          Lorem ipsum dolor sit amet consectetur. Odio ut lorem sapien maecenas
+          lectus lobortis. Vulputate mattis penatibus sit placerat. Sed varius
+          nibh massa ut fringilla amet. Massa tempor nibh condimentum
+          pellentesque mauris ullamcorper.
+        </p>
+        <button className="rounded-full border-[1px] font-bold text-[14px] border-white max-h-full max-w-full w-[129px] h-[42px] bg-gradient-to-r from-[#2662D6] to-[#002241] text-white">
+          Learn More
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Education;
